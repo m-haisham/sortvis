126c8b7dd46cac7c1e41281d8dfd0b4bdf381684
mHaisham <mHaisham@users.noreply.github.com>
2020-07-13 00:58:59 +0500

    Import Fixes

--- visualizer/sorting/__init__.py
+++ visualizer/sorting/__init__.py
@@ -1,4 +1,2 @@
-from visualizer.sorting.algorithms.algorithm import Algorithm
-
 from .controller import AlgorithmController
 from .callback import CallbackList
--- visualizer/sorting/algorithms/__init__.py
+++ visualizer/sorting/algorithms/__init__.py
@@ -1,3 +1,4 @@
+from .algorithm import Algorithm
 from .cocktail import CocktailSort
 from .insertion import InsertionSort
 from .cycle import CycleSort
